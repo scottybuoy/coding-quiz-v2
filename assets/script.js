d0ec1994e0c00382e26bb9fef5d9357e9e9e18f5
Scott Everett <burningcupidxxx@gmail.com>
2022-01-05 16:56:32 -0800

    Creater functioning timer

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
 var currentTime = 50;
+var timerLabel = $("#timer-label");
+var timerCountdown = $("#timer-countdown");
+var clock;
 var main = document.querySelector("#card");
+var card = $("#card");
 var timer = document.querySelector("#timer-container");
 var button1 = $("<button>", {"id" : "button1", "class" : "btn"});
 var button2 = $("<button>", {"id" : "button2", "class" : "btn"});
 var button3 = $("<button>", {"id" : "button3", "class" : "btn"});
 var button4 = $("<button>", {"id" : "button4", "class" : "btn"});
 var header1 = $("#main-head");
+var desc = $("<p>", {"id" : "description2"});
 
 
 
@@ -76,6 +81,18 @@ $("#start-button").on("click", transition1);
 
 
 function transition1() {
+
+    clock = setInterval(tickTock, 1000);
+    timerCountdown.text(currentTime);
+
+    function tickTock() {
+        timerCountdown.text(currentTime);
+        currentTime --;
+    };
+
+
+
+
     document.body.insertBefore(timer, main);
     $("#start-button").remove();
     $("#description").remove();
@@ -86,5 +103,62 @@ function transition1() {
     button2.text(quizQuestions[0].answers.b);
     button3.text(quizQuestions[0].answers.c);
     button4.text(quizQuestions[0].answers.d);
+    $(".btn").on("click", transition2);
     
+}
+
+
+function transition2() {
+    header1.text(quizQuestions[1].question);
+    button1.text(quizQuestions[1].answers.a);
+    button2.text(quizQuestions[1].answers.b);
+    button3.text(quizQuestions[1].answers.c);
+    button4.text(quizQuestions[1].answers.d);
+    $(".btn").on("click", transition3);
+}
+
+
+
+
+function transition3() {
+    header1.text(quizQuestions[2].question);
+    button1.text(quizQuestions[2].answers.a);
+    button2.text(quizQuestions[2].answers.b);
+    button3.text(quizQuestions[2].answers.c);
+    button4.text(quizQuestions[2].answers.d);
+    $(".btn").on("click", transition4);
+}
+
+
+
+function transition4() {
+    header1.text(quizQuestions[3].question);
+    button1.text(quizQuestions[3].answers.a);
+    button2.text(quizQuestions[3].answers.b);
+    button3.remove();
+    button4.remove();
+    $(".btn").on("click", transition5);
+}
+
+
+
+
+function transition5() {
+    $("#buttonCont").append([button3, button4]);
+    header1.text(quizQuestions[4].question);
+    button1.text(quizQuestions[4].answers.a);
+    button2.text(quizQuestions[4].answers.b);
+    button3.text(quizQuestions[4].answers.c);
+    button4.text(quizQuestions[4].answers.d);
+    $(".btn").on("click", transition6);
+}
+
+
+function transition6() {
+    header1.attr("style", "text-align: center");
+    header1.text("Finished!");
+    $("#buttonCont").remove();
+    card.append(desc);
+    desc.text(`Your score is ${currentTime}`);
+    clearInterval(clock);
 }
